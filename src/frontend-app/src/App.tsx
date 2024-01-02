a8188a9d51d4b8ae14f5a12b9a31a5fafbe49230
Yavuz Tokus <yavuztokus2010@gmail.com>
2024-01-02 21:23:02 +0000

    results was fetched and spin element was added

--- src/frontend-app/src/App.tsx
+++ src/frontend-app/src/App.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
 import TrivyReport from "./components/trivy-report/TrivyReport";
+import { Spin } from "antd";
 
 function App() {
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const response = await fetch("../../../test/data/default/result.json");
+        const response = await fetch("/api/defaultResults.json");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log("Fetched Data:", data);
+        setResult(data.Results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <TrivyReport />
+      {result.length > 0 ? (
+        <TrivyReport />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <Spin size="large" />
+        </div>
+      )}
     </>
   );
 }
