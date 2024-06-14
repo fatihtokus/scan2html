781ef4ce49000ed1f326c5284be4133307f7d62f
Fatih Tokus <fatih.tokus@gmail.com>
2024-06-14 10:49:03 +0100

    feat: Load multiple reports from a Url as request parameters (#61)
    * feat: Select automatically the menu(vulns, secrets etc) that has data
    
    * Reading multiple reports via request parameter

--- src/frontend-app/src/components/trivy-report/UploadAReport.tsx
+++ src/frontend-app/src/components/trivy-report/UploadAReport.tsx
@@ -1,20 +1,59 @@
-import { Button, Upload } from "antd";
-import {UploadInfo } from "../../types";
-import { UploadOutlined} from "@ant-design/icons";
+import React from 'react';
+import { Button, Upload, Table } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import { UploadInfo } from '../../types';
 
 interface UploadAReportProps {
   onReportUpload: (info: UploadInfo) => void;
-  loadedReport: string;
+  loadedReportFiles: string[];
+  manuallyLoadedReportFile: string;
 }
 
-const UploadAReport: React.FC<UploadAReportProps> = ({ onReportUpload, loadedReport }) => {
+const UploadAReport: React.FC<UploadAReportProps> = ({
+  onReportUpload,
+  loadedReportFiles,
+  manuallyLoadedReportFile,
+}) => {
+  const columns = [
+    {
+      title: 'Loaded Report Name',
+      dataIndex: 'fileName',
+      key: 'fileName',
+    },
+  ];
 
-return (
-    <>
-      <Upload onChange={onReportUpload} accept=".json" showUploadList={false} beforeUpload={() => false}>
-          <Button icon={<UploadOutlined />}>Select a Trivy JSON Report from your local file system</Button> {loadedReport}
+  // Create a new array including manuallyLoadedReportFile if it is not an empty string
+  const reportFiles = manuallyLoadedReportFile 
+    ? [...loadedReportFiles, manuallyLoadedReportFile]
+    : loadedReportFiles;
+
+  const dataSource = reportFiles.map((fileName, index) => ({
+    key: index,
+    index: index + 1,
+    fileName: fileName,
+  }));
+
+  return (
+    <div>
+      <Upload
+        onChange={onReportUpload}
+        accept=".json"
+        showUploadList={false}
+        beforeUpload={() => false}
+      >
+        <Button icon={<UploadOutlined />}>
+          Select a Trivy JSON Report from your local file system
+        </Button>
       </Upload>
-    </>
+      <div>
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          pagination={false}
+          style={{ marginTop: 20 }}
+        />
+      </div>
+    </div>
   );
 };
 
