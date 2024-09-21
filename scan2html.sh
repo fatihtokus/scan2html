6365f270ca62565ba034e92e5bab0dcde4d4af82
tokufat <fatih.tokus@gmail.com>
2024-09-21 21:29:22 +0100

    wip

--- scan2html.sh
+++ scan2html.sh
@@ -30,7 +30,7 @@ Examples:
   # Scan and generate SBOM(spdx) report
   trivy scan2html image --format spdx alpine:3.15 interactive_result.html
 
-  # Generate report from multiple scan results
+  # Generate report from multiple scan results - experimental
   trivy scan2html generate interactive_result.html from vulnerabilities.json misconfigs.json secrets.json
 
 EOS
