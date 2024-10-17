2bce21f500d1a9c9d7b864fffa51d77f621dab47
tokufat <fatih.tokus@gmail.com>
2024-10-17 17:50:13 +0100

    EpssData From Ui

--- src/frontend-app/src/App.tsx
+++ src/frontend-app/src/App.tsx
@@ -148,6 +148,7 @@ function App() {
     }
 
     setFilledResultsPerCategory(tempResults);
+
     setMenuItems([
       { key: "vulnerabilities", icon: <BugOutlined />, label: `Vulnerabilities (${vulnerabilities.length})` },
       { key: "secrets", icon: <LockOutlined />, label: `Secrets (${secrets.length})` },
